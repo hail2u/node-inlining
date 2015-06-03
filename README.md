94c431741763f72e91c4ebaaa5924abf430c85e3
Kyo Nagashima <hail2u@gmail.com>
2015-06-03 16:56:04 +0900

    Implement basic functions

---
+++ README.md
@@ -0,0 +1,22 @@
+inlining
+========
+
+Inline external CSS to a HTML file.
+
+
+INSTALL
+-------
+
+    $ npm install -g hail2u/node-inlining
+
+
+USAGE
+-----
+
+    $ inlining input.html input.css >output.html
+
+
+LICENSE
+-------
+
+MIT: http://hail2u.mit-license.org/2015
---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var jsdom = require("jsdom");
+var postcss = require("postcss");
+
+var list = postcss.list;
+
+function buildCSSText(decls) {
+  var cssText = "";
+  decls.forEach(function (decl) {
+    if (decl.type !== "decl") {
+      return;
+    }
+
+    cssText += decl.prop + ":" + decl.value + ";";
+  });
+
+  return cssText;
+}
+
+module.exports = function (css, html, callback) {
+  jsdom.env(html, function (errors, window) {
+    var document = window.document;
+    postcss.parse(css).eachRule(function (rule) {
+      var cssText = buildCSSText(rule.nodes);
+      list.comma(rule.selector).forEach(function (selector) {
+        var elm;
+        var elms = document.querySelectorAll(selector);
+        var i;
+        var l = elms.length;
+
+        for (i = 0; i < l; i++) {
+          elm = elms[i];
+          elm.style.cssText += cssText;
+        }
+      });
+    });
+
+    callback(document.documentElement.innerHTML);
+  });
+};
