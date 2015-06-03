8c208ef704cb58709712066c093589e192df243a
Kyo Nagashima <hail2u@gmail.com>
2015-06-03 17:43:25 +0900

    Insert remaining CSS into `head`

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ module.exports = function (css, html, callback) {
   jsdom.env(html, function (errors, window) {
     var document = window.document;
     var body = document.body;
-    postcss.parse(css).eachRule(function (rule) {
+    var remain = document.createElement("style");
+    var root = postcss.parse(css);
+    root.eachRule(function (rule) {
+      if (rule.parent.type !== "root") {
+        return;
+      }
+
       var cssText = buildCSSText(rule.nodes);
       list.comma(rule.selector).forEach(function (selector) {
         var elm;
@@ -40,8 +46,12 @@ module.exports = function (css, html, callback) {
           elm.style.cssText += cssText;
         }
       });
+
+      rule.removeSelf();
     });
 
+    remain.appendChild(document.createTextNode(root.toString()));
+    document.head.appendChild(remain);
     callback(document.documentElement.innerHTML);
   });
 };
