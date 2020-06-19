1a96bb2d99becceca182758dd3aaaf3e1c9056bc
Kyo Nagashima <hail2u@gmail.com>
2020-06-19 15:25:13 +0900

    Refactor

--- bin/inlining.js
+++ bin/inlining.js
@@ -1,49 +1,52 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const inlining = require("..");
+const inlining = require("../index");
 
 const pkg = require("../package.json");
-const [binname] = Object.keys(pkg.bin);
-const [,, html] = process.argv;
 
-switch (html) {
-	case "--version":
-	case "-v":
+const toSTDOUT = (res) => {
+	console.log(res);
+};
+
+const main = () => {
+	const [binname] = Object.keys(pkg.bin);
+	const file = process.argv.slice(2).shift();
+
+	if (file === "--version" || file === "-v") {
 		console.log(`${binname} v${pkg.version}`);
-		break;
+		return;
+	}
 
-	case "--help":
-	case "-h":
+	if (file === "--help" || file === "-h") {
 		console.log(`Usage: ${binname} INPUT
 
 Description:
-  ${pkg.description}
+	${pkg.description}
 
 Options:
-  -h, --help     Show this message.
-  -v, --version  Print version information.
+	-h, --help     Show this message.
+	-v, --version  Print version information.
 
 Use a single dash for INPUT to read CSS from standard input.
 `);
-		break;
+		return;
+	}
 
-	case "-":
+	if (file === "-") {
 		inlining(
 			fs.readFileSync(process.stdin.fd, "utf8"),
 			"index.html",
-			(res) => {
-				console.log(res);
-			}
+			toSTDOUT
 		);
-		break;
+		return;
+	}
 
-	default:
-		inlining(
-			fs.readFileSync(html, "utf8"),
-			html,
-			(res) => {
-				console.log(res);
-			}
-		);
-}
+	inlining(
+		fs.readFileSync(file, "utf8"),
+		file,
+		toSTDOUT
+	);
+};
+
+main();
--- index.js
+++ index.js
@@ -13,163 +13,145 @@ const {
 	list
 } = postcss;
 
-function toDataURL(filepath) {
-	return `data:${mime.getType(filepath)};base64,${fs.readFileSync(filepath).toString("base64")}`;
-}
+const toHEXColor = (value) => {
+	if (/^#[0-9a-f]{3}$/i.test(value)) {
+		return onecolor(value).hex();
+	}
 
-function inlineImage(value, root) {
+	return value;
+};
+
+const normalizeColor = (value) => list.space(value)
+	.map(toHEXColor)
+	.join(" ");
+
+const toDataURL = (filepath) => {
+	const mimeType = mime.getType(filepath);
+	const base64 = fs.readFileSync(filepath)
+		.toString("base64");
+	return `data:${mimeType};base64,${base64}`;
+};
+
+const inlineImage = (dir, value) => {
 	const url = balanced("url(", ")", value);
 
 	if (!url) {
 		return value;
 	}
 
-	const filepath = path.resolve(root, url.body.replace(/^\s*("|')?\s*(.*)\s*\1\s*$/, "$2"));
+	const filepath = path.resolve(dir, url.body.replace(/^\s*("|')?\s*(.*)\s*\1\s*$/, "$2"));
 
 	if (fs.existsSync(filepath)) {
 		url.body = toDataURL(filepath);
 	}
 
 	return `${url.pre}url(${url.body})${url.post}`;
-}
+};
 
-function toHEXColor(value) {
-	if (/^#[0-9a-f]{3}$/i.test(value)) {
-		return onecolor(value).hex();
+const buildCSSText = (dir, decl) => {
+	if (decl.type !== "decl") {
+		return "";
 	}
 
-	return value;
+	const value = list.comma(decl.value)
+		.map(normalizeColor)
+		.map(inlineImage.bind(null, dir))
+		.join(",");
+
+	if (!decl.important) {
+		return `${decl.prop}:${value}`;
+	}
+
+	return `${decl.prop}:${value} !important`;
 }
 
-function normalizeColor(value) {
-	return list.space(value).map(toHEXColor).join(" ");
-}
+const inlineImages = (dir, decl) => {
+	decl.value = inlineImage(dir, decl.value);
+};
 
-function buildCSSText(decls, root) {
-	let cssText = "";
-	decls.forEach((decl) => {
-		if (decl.type !== "decl") {
-			return;
+const spreadToElements = (document, cssText, selector) => {
+	let elms = "";
+
+	try {
+		elms = document.querySelectorAll(selector);
+	} catch {
+		return;
+	}
+
+	for (const elm of elms) {
+		if (elm !== document.body && !document.body.contains(elm)) {
+			continue;
 		}
 
-		cssText += `${decl.prop}:${list.comma(decl.value).map((v) => inlineImage(normalizeColor(v), root)).join(",")}`;
+		const style = elm.getAttribute("style");
 
-		if (decl.important) {
-			cssText += " !important";
+		if (!style) {
+			elm.setAttribute("style", `${cssText};`);
+			continue;
 		}
 
-		cssText += ";";
-	});
-	return cssText;
-}
+		elm.setAttribute("style", `${style}${cssText};`);
+	}
+};
 
-function inlineCSS(css, pathCSS, document) {
-	const dir = path.dirname(pathCSS);
-	const {body} = document;
-	const remain = document.createElement("style");
+const processRule = (dir, document, rule) => {
+	if (rule.parent.type !== "root") {
+		rule.nodes.forEach(inlineImages.bind(null, dir));
+		return;
+	}
+
+	const cssText = rule.nodes.map(buildCSSText.bind(null, dir))
+		.join(";");
+	list.comma(rule.selector)
+		.forEach(spreadToElements.bind(null, document, cssText));
+	rule.remove();
+};
+
+const inlineCSS = (css, filepath, document) => {
 	const root = postcss.parse(css);
-	root.walkRules((rule) => {
-		let cssText = "";
-
-		if (rule.parent.type !== "root") {
-			rule.nodes.forEach((decl) => {
-				decl.value = inlineImage(decl.value, dir);
-			});
-
-			return;
-		}
-
-		cssText = buildCSSText(rule.nodes, dir);
-		list.comma(rule.selector).forEach((selector) => {
-			let elms = "";
-			let l = "";
-			let i = "";
-			let elm = "";
-			let style = "";
-
-			try {
-				elms = document.querySelectorAll(selector);
-			} catch (e) {
-				return;
-			}
-
-			l = elms.length;
-
-			for (i = 0; i < l; i++) {
-				elm = elms[i];
-
-				if (elm !== body && !body.contains(elm)) {
-					continue;
-				}
-
-				style = elm.getAttribute("style");
-
-				if (!style) {
-					style = "";
-				}
-
-				elm.setAttribute("style", style + cssText);
-			}
-		});
-		rule.remove();
-	});
+	const dir = path.dirname(filepath);
+	root.walkRules(processRule.bind(null, dir, document));
 
 	if (root.nodes.length > 0) {
-		remain.appendChild(document.createTextNode(root.toString()));
-		document.head.appendChild(remain);
+		const remain = document.createElement("style");
+		remain.append(root.toString());
+		document.head.append(remain);
 	}
+};
 
-	return document;
-}
-
-module.exports = function (html, filepath, callback) {
+module.exports = (html, filepath, callback) => {
 	const dom = new JSDOM(html);
-	let {
+	const {
 		document
 	} = dom.window;
-	let elms = document.querySelectorAll("[style]");
-	let l = elms.length;
-	let i = 0;
-	let elm = "";
-	const links = document.querySelectorAll('link[rel="stylesheet"]');
-	let href = "";
-	const dir = path.dirname(filepath);
 
-	for (; i < l; i++) {
-		elm = elms[i];
+	for (const elm of document.querySelectorAll("[style]")) {
 		elm.setAttribute("_style", elm.getAttribute("style"));
 		elm.setAttribute("style", "");
 	}
 
-	l = links.length;
+	const dir = path.dirname(filepath);
 
-	for (i = 0; i < l; i++) {
-		elm = links[i];
-		href = path.resolve(dir, elm.href);
+	for (const elm of document.querySelectorAll('link[rel="stylesheet"]')) {
+		const href = path.resolve(dir, elm.href);
 
 		if (fs.existsSync(href)) {
 			elm.parentNode.removeChild(elm);
-			document = inlineCSS(fs.readFileSync(href, "utf8"), href, document);
+			const css = fs.readFileSync(href, "utf8");
+			inlineCSS(css, href, document);
 		}
 	}
 
-	l = elms.length;
-
-	for (i = 0; i < l; i++) {
-		elm = elms[i];
+	for (const elm of document.querySelectorAll("[_style]")) {
 		elm.setAttribute(
 			"style",
-			elm.getAttribute("style") + elm.getAttribute("_style")
+			`${elm.getAttribute("style")}${elm.getAttribute("_style")}`
 		);
 		elm.removeAttribute("_style");
 	}
 
-	elms = document.images;
-	l = elms.length;
-
-	for (i = 0; i < l; i++) {
-		elm = elms[i];
-		href = path.resolve(dir, elm.src.replace(/^file:\/\//, ""));
+	for (const elm of document.images) {
+		const href = path.resolve(dir, elm.src.replace(/^file:\/\//, ""));
 
 		if (fs.existsSync(href)) {
 			elm.src = toDataURL(href);
